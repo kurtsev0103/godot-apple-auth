01cfbd9e36c70710451a8e4844e61ea13a13805d
Oleksandr Kurtsev <kurtsev0103@gmail.com>
2021-09-11 22:03:06 +0200

    Added GodotAppleAuth static library

---
+++ GodotAppleAuth/GodotAppleAuth.h
@@ -0,0 +1,12 @@
+//
+//  GodotAppleAuth.h
+//  GodotAppleAuth
+//
+//  Created by Oleksandr Kurtsev on 11/09/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GodotAppleAuth : NSObject
+
+@end
