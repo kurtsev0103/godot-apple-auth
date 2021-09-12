005a03b8dbc9dcffa1529eca741d0215f5a2bda1
Oleksandr Kurtsev <kurtsev0103@gmail.com>
2021-09-12 11:50:39 +0200

    Bump version to 1.0.0

--- GodotAppleAuth/GodotAppleAuth.h
+++ GodotAppleAuth/GodotAppleAuth.h
@@ -8,6 +8,8 @@
 #import "core/engine.h"
 
 #define PLUGIN_NAME "GodotAppleAuth"
+#define PLUGIN_VERSION "1.0.0"
+
 #define SIGNAL_CREDENTIAL "credential"
 #define SIGNAL_AUTHORIZATION "authorization"
 
